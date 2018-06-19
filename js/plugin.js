89aaf852045bdf70b120751cce9b24db2b768d25
Asmaa Badran <Asmaa.mo7ammed@gmail.com>
2018-06-19 02:41:36 +0200

    clean js codes

--- js/plugin.js
+++ js/plugin.js
@@ -1,21 +1,23 @@
-var slideIndex = 1;
+var slideIndex = 1,
+    i;
 showSlides(slideIndex);
 
-
-function plusSlides(n){
+function plusSlides(n) {
+    "use strict";
     showSlides(slideIndex += n);
 }
 
-function showSlides(n){
+function showSlides(n) {
+    "use strict";
     var slides = document.getElementsByClassName("slide");
-    if(n > slides.length){
+    if (n > slides.length) {
         slideIndex = 1;
     }
-    if (n < 1){
+    if (n < 1) {
         slideIndex = slides.length;
     }
-    for(var i = 0 ; i < slides.length ; i++){
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
 }
